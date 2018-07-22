114f1eefa81574a9c89eef908e84703b797e3fb9
Nessiezhao <Nessie_zhao@163.com>
2018-07-22 21:53:03 +0800

    add test3

--- test3/test.cpp
+++ test3/test.cpp
@@ -1,28 +1,44 @@
 #include <iostream>
-class Person
+class Date
 {
 public:
-    void Display()
+    Date()
+    {}
+    Date(int year,int month,int day)
     {
-        std::cout<<_name<<"-"<<_sex<<"-"<<_age<<std::endl;
+        _year = year;
+        _month = month;
+        _day = day;
     }
-public:
-    const char* _name;
-    const char* _sex;
-    int _age;
+private:
+    int _year;
+    int _month;
+    int _day;
 };
-
-void Test()
-{
-    Person p;
-    p._name = "Jack";
-    p._age = 10;
-    p._sex = "男";
-    p.Display();
-}
-
-int main()
-{
-    Test();
-    return 0;
-}
+//class Person
+//{
+//public:
+//    void Display()
+//    {
+//        std::cout<<_name<<"-"<<_sex<<"-"<<_age<<std::endl;
+//    }
+//public:
+//    const char* _name;
+//    const char* _sex;
+//    int _age;
+//};
+//
+//void Test()
+//{
+//    Person p;
+//    p._name = "Jack";
+//    p._age = 10;
+//    p._sex = "男";
+//    p.Display();
+//}
+//
+//int main()
+//{
+//    Test();
+//    return 0;
+//}
