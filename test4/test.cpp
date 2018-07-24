7397a32c83ed8755fb3037aa11c245c5ed987ad2
Nessiezhao <Nessie_zhao@163.com>
2018-07-24 10:29:11 +0800

    add test4

--- test4/test.cpp
+++ test4/test.cpp
@@ -25,26 +25,37 @@ private:
 class Date
 {
 public:
-//    Date(int year,int month,int day,const Time& t)
-//    {
-//        std::cout<<"Date()--非初始化列表"<<std::endl;
-//        _year = year;
-//        _month = month;
-//        _day = day;
-//        _t = t;
-//    }
     Date(int year,int month,int day,const Time& t)
-        :_year(year)
-         ,_month(month)
-         ,_day(day)
-         ,_t(t)
     {
         std::cout<<"Date()--非初始化列表"<<std::endl;
+        _year = year;
+        _month = month;
+        _day = day;
+        _t = t;
     }
-
+//    Date(int year,int month,int day,const Time& t)
+//        :_year(year)
+//         ,_month(month)
+//         ,_day(day)
+//         ,_t(t)
+//    {
+//        std::cout<<"Date()--初始化列表"<<std::endl;
+//    }
+//
 private:
     int _year;
     int _month;
     int _day;
     Time _t;
 };
+void Test()
+{
+    Time t1;
+    Date d1(2015,4,29,t1);
+}
+
+int main()
+{
+    Test();
+    return 0;
+}
