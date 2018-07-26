d8bdc1817a27fd7aa59f01755bf7792d25369957
Nessiezhao <Nessie_zhao@163.com>
2018-07-26 10:01:26 +0800

    add List.cpp

--- test6/List.cpp
+++ test6/List.cpp
@@ -1,4 +1,71 @@
 #pragma once
 #include<iostream>
 
+typedef int DateType;
 
+struct ListNode
+{
+    ListNode* _next;
+    ListNode* _prev;
+    DateType _data;
+    ListNode(DateType x)
+        :_data(x)
+         ,_next(NULL)
+         ,_prev(NULL)
+    {}
+};
+
+class List
+{
+    typedef ListNode Node;
+public:
+    List()
+        :_head(new Node(DateType()))
+    {
+        _head->_next = _head;
+        _head->_prev = _head;
+    }
+    List(const List& l)
+    {
+
+    }
+    List& operator=(const List& l)
+    {
+
+    }
+    ~List()
+    {
+
+    }
+
+    void PushBack(DateType x)
+    {
+
+    }
+    void PushFront(DateType x)
+    {
+
+    }
+    void PopBack(DateType x)
+    {
+
+    }
+    void PopFront(DateType x)
+    {
+
+    }
+    Node* Find(DateType x)
+    {
+
+    }
+    void Insert(Node* pos,DateType x)
+    {
+
+    }
+    void Erase(Node* pos)
+    {
+
+    }
+private:
+    Node* _head;
+};
