6f3874680d258247e16014fdf5320f73619ca186
Nessiezhao <Nessie_zhao@163.com>
2018-07-27 16:49:59 +0800

    add test6

--- test6/Vector.cpp
+++ test6/Vector.cpp
@@ -44,21 +44,27 @@ public:
         }
         _first = _finish = _endofstorage = NULL;
     }
-    size_t Size()
+    size_t Size()const
     {
-
+        return (_finish - _first);
     }
-    size_t Capacity()
+    size_t Capacity()const
     {
-
+        return (_endofstorage - _first);
     }
     void Expand(size_t n)
     {
 
     }
-    void PushBack(DateType x)
+    void PushBack(const T& x)
     {
-
+        if(_finish == _endofstorage)
+        {
+            size_t newcapacity = Capacity() == 0 ? 3 : (Capacity()* 2);
+            Expand(newcapacity);
+        }
+        *_finish = x;
+        ++_finish;
     }
     void Reserve(size_t n)
     {
@@ -66,7 +72,7 @@ public:
     }
     void PopBack()
     {
-
+        Erase(Size() - 1);
     }
     void Insert(size_t pos,DateType x)
     {
