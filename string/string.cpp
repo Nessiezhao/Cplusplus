cbf6db8c7cd7dcb21f09e0042e0f6c76ec552565
Nessiezhao <Nessie_zhao@163.com>
2018-07-30 11:50:05 +0800

    add string.cpp

--- string/string.cpp
+++ string/string.cpp
@@ -69,79 +69,186 @@ using namespace std;
 //    char* _str;
 //    size_t* _pCount;
 //};
-
+//
+//class String
+//{
+//public:
+//    String(char* str="")
+//        :_str(new char(strlen(str)+1))
+//    {
+//        strcpy(_str,str);
+//    }
+//    String(const String& s)
+//        :_str(new char(strlen(s._str)+1))
+//    {
+//        strcpy(_str,s._str);
+//    }
+//    //s1 = s3
+//    String& operator=(const String& s)
+//    {
+//        if(this != &s)
+//        {
+//            char* tmp = new char(strlen(s._str) + 1);
+//            strcpy(tmp,s._str);
+//            delete[] _str;
+//            _str = tmp;
+//        }
+//        return *this;
+//    }
+//    ~String()
+//    {
+//        delete[] _str;
+//        _str = NULL;
+//    }
+//    const char* c_str()
+//    {
+//        return _str;
+//    }
+//    const char& operator[](size_t pos)const
+//    {
+//        return _str[pos];
+//    }
+//    char& operator[](size_t pos)
+//    {
+//        if(GetCount() > 1 )
+//        {
+//            char* tmp = new char(strlen(_str)+1+4);
+//            strcpy(_str,tmp+4);
+//            --GetCount();
+//            _str = tmp+4;
+//            GetCount() = 1;
+//        }
+//        return _str[pos];
+//    }
+//    int& GetCount()
+//    {
+//        return *((int*)_str - 1);
+//    }
+//private:
+//    char* _str;
+//};
+//
+//void Test()
+//{
+//    String s1("abcd");
+//    char* str1 = "abcd";
+//    cout<<sizeof(s1)<<endl;
+//    cout<<sizeof(str1)<<endl;
+//    String s2(s1);
+//    cout<<s1.c_str()<<endl;
+//    cout<<s2.c_str()<<endl;
+//    String s3("1234");
+//    s1 = s3;
+//    cout<<s1.c_str()<<endl;
+//    cout<<s3.c_str()<<endl;
+//}
 class String
 {
 public:
-    String(char* str="")
-        :_str(new char(strlen(str)+1))
+    String(const char* str="")
     {
-        strcpy(_str,str);
+
     }
+    //s1.Swap(s2)
+    void Swap(String& s)
+    {
+
+    }
+    //String s2(s1)
     String(const String& s)
-        :_str(new char(strlen(s._str)+1))
     {
-        strcpy(_str,s._str);
+
     }
-    //s1 = s3
-    String& operator=(const String& s)
+    //s1 = s2
+    String& operator=(String s)
     {
-        if(this != &s)
-        {
-            char* tmp = new char(strlen(s._str) + 1);
-            strcpy(tmp,s._str);
-            delete[] _str;
-            _str = tmp;
-        }
-        return *this;
+
     }
     ~String()
     {
-        delete[] _str;
-        _str = NULL;
+
     }
     const char* c_str()
     {
-        return _str;
+
     }
-    const char& operator[](size_t pos)const
+    void Expand(size_t n)
     {
-        return _str[pos];
+
     }
-    char& operator[](size_t pos)
+    void PushBack(const char* str)
     {
-        if(GetCount() > 1 )
-        {
-            char* tmp = new char(strlen(_str)+1+4);
-            strcpy(_str,tmp+4);
-            --GetCount();
-            _str = tmp+4;
-            GetCount() = 1;
-        }
-        return _str[pos];
+
     }
-    int& GetCount()
+    void PopBack()
     {
-        return *((int*)_str - 1);
+
+    }
+    void Insert(size_t pos,char ch)
+    {
+
+    }
+    void Insert(size_t pos,const char* str)
+    {
+
+    }
+    void Erase(size_t pos,size_t n = 1)
+    {
+        
+    }
+    size_t Find(char ch)
+    {
+
+    }
+    size_t Find(const char* str)
+    {
+
+    }
+    String operator+(char ch)
+    {
+
+    }
+    String& operator+=(char ch)
+    {
+
+    }
+    String operator+(const char* str)
+    {
+
+    }
+    String& operator+=(const char* str)
+    {
+        
+    }
+    bool operator>(const String& s)
+    {
+
+    }
+    bool operator>=(const String& s)
+    {
+
+    }
+    bool operator<(const String& s)
+    {
+
+    }
+    bool operator<=(const String& s)
+    {
+
+    }
+    bool operator==(const String& s)
+    {
+
+    }
+    bool operator!=(const String& s)
+    {
+
     }
 private:
     char* _str;
+    size_t _size;
+    size_t _capacity;
 };
-
-void Test()
-{
-    String s1("abcd");
-    char* str1 = "abcd";
-    cout<<sizeof(s1)<<endl;
-    cout<<sizeof(str1)<<endl;
-    String s2(s1);
-    cout<<s1.c_str()<<endl;
-    cout<<s2.c_str()<<endl;
-    String s3("1234");
-    s1 = s3;
-    cout<<s1.c_str()<<endl;
-    cout<<s3.c_str()<<endl;
-}
 int main()
 {
     Test();
