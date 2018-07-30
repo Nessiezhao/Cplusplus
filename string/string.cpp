0054c70099d591966dfd4ec45e3422f912178e6f
Nessiezhao <Nessie_zhao@163.com>
2018-07-30 11:20:16 +0800

    add string

--- string/string.cpp
+++ string/string.cpp
@@ -2,112 +2,132 @@
 #include<string.h>
 using namespace std;
 
-class String
-{
-public:
-    String(char* str="")
-    {
-        _str = new char(strlen(str) + 1);
-        strcpy(_str,str);
-        _pCount = new size_t(1);
-    }
-    String(const String& s)
-    {
-        _str = s._str;
-        _pCount = s._pCount;
-        ++(*_pCount);
-    }
-    String& operator=(const String& s)
-    {
-        //if(this!= &s)
-        if(_str != s._str)
-        {
-            if(--(*_pCount) == 0)
-            {
-                delete[] _str;
-                delete _pCount;
-            }
-            _str = s._str;
-            _pCount = s._pCount;
-            ++(*_pCount);
-        }
-    }
-    ~String()
-    {
-        if(--(*_pCount) == 0)
-        {
-            delete[] _str;
-            delete _pCount;
-        }
-    }
-    const char* c_str()
-    {
-        return _str;
-    }
-    void CopyOnWrite()
-    {
-        if(*_pCount > 1)
-        {
-            char* tmp = new char(strlen(_str)+1);
-            strcpy(tmp,_str);
-            --(*_pCount);
-
-            _str = tmp;
-            _pCount = new size_t(1);
-        }
-    }
-    char& operator[](size_t pos)
-    {
-        CopyOnWrite();
-        return _str[pos];
-    }
-    const char& operator[](size_t pos)const
-    {
-        return _str[pos];
-    }
-private:
-    char* _str;
-    size_t* _pCount;
-};
-
 //class String
 //{
 //public:
 //    String(char* str="")
-//        :_str(new char(strlen(str)+1))
 //    {
+//        _str = new char(strlen(str) + 1);
 //        strcpy(_str,str);
+//        _pCount = new size_t(1);
 //    }
 //    String(const String& s)
-//        :_str(new char(strlen(s._str)+1))
 //    {
-//        strcpy(_str,s._str);
+//        _str = s._str;
+//        _pCount = s._pCount;
+//        ++(*_pCount);
 //    }
-//    //s1 = s3
 //    String& operator=(const String& s)
 //    {
-//        if(this != &s)
+//        //if(this!= &s)
+//        if(_str != s._str)
 //        {
-//            char* tmp = new char(strlen(s._str) + 1);
-//            strcpy(tmp,s._str);
-//            delete[] _str;
-//            _str = tmp;
+//            if(--(*_pCount) == 0)
+//            {
+//                delete[] _str;
+//                delete _pCount;
+//            }
+//            _str = s._str;
+//            _pCount = s._pCount;
+//            ++(*_pCount);
 //        }
-//        return *this;
 //    }
 //    ~String()
 //    {
-//        delete[] _str;
-//        _str = NULL;
+//        if(--(*_pCount) == 0)
+//        {
+//            delete[] _str;
+//            delete _pCount;
+//        }
 //    }
 //    const char* c_str()
 //    {
 //        return _str;
 //    }
+//    void CopyOnWrite()
+//    {
+//        if(*_pCount > 1)
+//        {
+//            char* tmp = new char(strlen(_str)+1);
+//            strcpy(tmp,_str);
+//            --(*_pCount);
+//
+//            _str = tmp;
+//            _pCount = new size_t(1);
+//        }
+//    }
+//    char& operator[](size_t pos)
+//    {
+//        CopyOnWrite();
+//        return _str[pos];
+//    }
+//    const char& operator[](size_t pos)const
+//    {
+//        return _str[pos];
+//    }
 //private:
 //    char* _str;
+//    size_t* _pCount;
 //};
-//
+
+class String
+{
+public:
+    String(char* str="")
+        :_str(new char(strlen(str)+1))
+    {
+        strcpy(_str,str);
+    }
+    String(const String& s)
+        :_str(new char(strlen(s._str)+1))
+    {
+        strcpy(_str,s._str);
+    }
+    //s1 = s3
+    String& operator=(const String& s)
+    {
+        if(this != &s)
+        {
+            char* tmp = new char(strlen(s._str) + 1);
+            strcpy(tmp,s._str);
+            delete[] _str;
+            _str = tmp;
+        }
+        return *this;
+    }
+    ~String()
+    {
+        delete[] _str;
+        _str = NULL;
+    }
+    const char* c_str()
+    {
+        return _str;
+    }
+    const char& operator[](size_t pos)const
+    {
+        return _str[pos];
+    }
+    char& operator[](size_t pos)
+    {
+        if(GetCount() > 1 )
+        {
+            char* tmp = new char(strlen(_str)+1+4);
+            strcpy(_str,tmp+4);
+            --GetCount();
+            _str = tmp+4;
+            GetCount() = 1;
+        }
+        return _str[pos];
+    }
+    int& GetCount()
+    {
+        return *((int*)_str - 1);
+    }
+private:
+    char* _str;
+};
+
 void Test()
 {
     String s1("abcd");
